2c09f874684b7cd0e3b2cb1a5a422d388344a187
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-03-20 14:26:24 +0100

    [add] calcolo del prezzo

---
+++ assets/js/scrypt.js
@@ -0,0 +1,7 @@
+
+const distanza = 55 //prompt('A quanti km dista la tua destinazione?');
+const age = 28 //prompt('Inserisci età');
+
+let prezzoTicket = Math.ceil(distanza * 0.21).toFixed(2) + '\u20AC'; 
+
+console.log('prezzoTicket', prezzoTicket);
